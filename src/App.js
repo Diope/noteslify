60d6d17366789088a3a79c17b78f0310dad89f73
Dion Pettaway <dionp@fullsail.edu>
2021-05-03 14:58:29 -0400

    Set up the folder structure and skeleton for useReducer as well getting listNotes and displaying them

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React,{useEffect, useReducer} from 'react'
+import {API} from 'aws-amplify';
+import {List} from 'antd';
+import {listNotes} from './graphql/queries'
+
+import {SET_NOTES, ERROR} from './store/constants'
+import {initialState} from './store/state'
+
+import 'antd/dist/antd.css';
 import './App.css';
 
+function reducer(state, {type, notes}) {
+  switch(type) {
+    case SET_NOTES:
+      return {...state, notes, loading: false }
+    case ERROR:
+      return {...state, loading: false, error: true}
+    default:
+      return state
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+  
+
+  async function fetchNotes() {
+    try {
+      const notesData = await API.graphql({
+        query: listNotes
+      })
+      dispatch({ type: SET_NOTES, notes: notesData.data.listNotes.items})
+    } catch (err) {
+      console.log('error: ', err)
+      dispatch({type: ERROR})
+    }
+  }
+
+  function renderItem(item) {
+    return (
+      <List.Item style={styles.item}>
+        <List.Item.Meta
+          title={item.name}
+          description={item.description}
+        />
+      </List.Item>
+    )
+  }
+
+  const styles = {
+    container: {padding: 20},
+    input: {marginBottom: 10},
+    item: {textAlign: 'left'},
+    p: {color: '#a290ff'}
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <List
+        loading={state.loading}
+        dataSource={state.notes}
+        renderItem={renderItem}
+      />
     </div>
   );
 }
---
+++ src/store/state.js
@@ -0,0 +1,6 @@
+export const initialState = {
+    notes: [],
+    loading: true,
+    error: false,
+    form: {name: "", description: ""}
+}
